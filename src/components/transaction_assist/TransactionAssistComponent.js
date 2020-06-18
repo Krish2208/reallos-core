e1bf69891d03a4db83f4e6ee54b1a8aec2993e6c
Akshat <sharmaakshat212000@gmail.com>
2020-06-18 05:22:03 -0500

    Transaction assist component completed

--- src/components/transaction_assist/TransactionAssistComponent.js
+++ src/components/transaction_assist/TransactionAssistComponent.js
@@ -9,12 +9,20 @@ import {
     ExpansionPanel,
     ExpansionPanelSummary,
     ExpansionPanelDetails, 
-    Divider
+    Divider,
+    Typography
 } from '@material-ui/core';
 import {
     TriangleDownIcon,
-    RepoIcon, 
-    CheckIcon
+    CheckIcon,
+    SearchIcon,
+    DotFillIcon,
+    ArchiveIcon,
+    EyeIcon,
+    ShieldIcon,
+    HomeIcon,
+    IssueClosedIcon,
+    SmileyIcon
 } from '@primer/octicons-react';
 import './transactionassist.css';
 class TransactionAssist extends Component{
@@ -64,31 +72,267 @@ class TransactionAssist extends Component{
                 <Grid item>
                     <ExpansionPanel>
                         <ExpansionPanelSummary expandIcon={<TriangleDownIcon />} > 
-                            <Grid container direction="row" alignItems="center">
+                            <Grid container direction="row" alignItems="center" spacing={4}>
                                 <Grid item>
-                                    <CheckIcon />
+                                    <CheckIcon size={25}/> 
                                 </Grid>
+                                <Divider orientation="vertical" className="expansion-divider"/>
                                 <Grid item>
-                                    <Divider orientation="vertical" />
-                                </Grid>
-                                <Grid item>
-                                    <p>text here</p>
+                                    <Grid container direction="row" alignItems="center" spacing={3}>
+                                        <Grid item>
+                                            <SearchIcon size={25}/>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography className="expansion-panel-heading">Title Search and Insurance</Typography>
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </ExpansionPanelSummary>
                         <ExpansionPanelDetails>
-                            <p>hey hey hey</p>
+                            <Grid container direction="column" justify="center">
+                                <Grid item>
+                                    <h2>
+                                        Title goes here
+                                    </h2>
+                                </Grid>
+                                <Grid item>
+                                    <Typography className="expansion-panel-text">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius auctor tincidunt. 
+                                    Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                    </Typography>
+                                    <Typography className="expansion-panel-text">
+                                    Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="contained" className="action-button">Action</Button>
+                                </Grid>
+                            </Grid>
                         </ExpansionPanelDetails>
                     </ExpansionPanel>
                 </Grid>
+    
                 <Grid item>
                     <ExpansionPanel>
-                    <ExpansionPanelSummary expandIcon={<TriangleDownIcon />} > Panel 1 </ExpansionPanelSummary>
-                        <ExpansionPanelDetails>
-                            <p>hey hey hey</p>
-                        </ExpansionPanelDetails>
-                    </ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<TriangleDownIcon />} > 
+                                <Grid container direction="row" alignItems="center" spacing={4}>
+                                    <Grid item>
+                                        <DotFillIcon size={25} /> 
+                                    </Grid>
+                                    <Divider orientation="vertical" className="expansion-divider"/>
+                                    <Grid item>
+                                        <Grid container direction="row" alignItems="center" spacing={3}>
+                                            <Grid item>
+                                                <HomeIcon size={25} />
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className="expansion-panel-heading">Home Appraisal</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid container direction="column" justify="center">
+                                    <Grid item>
+                                        <h2>
+                                            Title goes here
+                                        </h2>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography className="expansion-panel-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius auctor tincidunt. 
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                        <Typography className="expansion-panel-text">
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" className="action-button">Action</Button>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
                 </Grid>
+
+                <Grid item>
+                    <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<TriangleDownIcon />} > 
+                                <Grid container direction="row" alignItems="center" spacing={4}>
+                                    <Grid item>
+                                        <DotFillIcon size={25}/> 
+                                    </Grid>
+                                    <Divider orientation="vertical" className="expansion-divider"/>
+                                    <Grid item>
+                                        <Grid container direction="row" alignItems="center" spacing={3}>
+                                            <Grid item>
+                                                <EyeIcon size={25}/>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className="expansion-panel-heading">Home Inspection</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid container direction="column" justify="center">
+                                    <Grid item>
+                                        <h2>
+                                            Title goes here
+                                        </h2>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography className="expansion-panel-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius auctor tincidunt. 
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                        <Typography className="expansion-panel-text">
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" className="action-button">Action</Button>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                </Grid>
+
+                <Grid item>
+                    <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<TriangleDownIcon />} > 
+                                <Grid container direction="row" alignItems="center" spacing={4}>
+                                    <Grid item>
+                                        <DotFillIcon size={25}/> 
+                                    </Grid>
+                                    <Divider orientation="vertical" className="expansion-divider"/>
+                                    <Grid item>
+                                        <Grid container direction="row" alignItems="center" spacing={3}>
+                                            <Grid item>
+                                                <ShieldIcon size={25}/>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className="expansion-panel-heading">Home Appraisal</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid container direction="column" justify="center">
+                                    <Grid item>
+                                        <h2>
+                                            Title goes here
+                                        </h2>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography className="expansion-panel-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius auctor tincidunt. 
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                        <Typography className="expansion-panel-text">
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" className="action-button">Action</Button>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                </Grid>
+
+                <Grid item>
+                    <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<TriangleDownIcon />} > 
+                                <Grid container direction="row" alignItems="center" spacing={4}>
+                                    <Grid item>
+                                        <DotFillIcon size={25}/> 
+                                    </Grid>
+                                    <Divider orientation="vertical" className="expansion-divider"/>
+                                    <Grid item>
+                                        <Grid container direction="row" alignItems="center" spacing={3}>
+                                            <Grid item>
+                                                <IssueClosedIcon size={25}/>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className="expansion-panel-heading">Pre-Closing</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid container direction="column" justify="center">
+                                    <Grid item>
+                                        <h2>
+                                            Title goes here
+                                        </h2>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography className="expansion-panel-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius auctor tincidunt. 
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                        <Typography className="expansion-panel-text">
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" className="action-button">Action</Button>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                </Grid>
+
+                <Grid item>
+                    <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<TriangleDownIcon />} > 
+                                <Grid container direction="row" alignItems="center" spacing={4}>
+                                    <Grid item>
+                                        <DotFillIcon size={25}/> 
+                                    </Grid>
+                                    <Divider orientation="vertical" className="expansion-divider"/>
+                                    <Grid item>
+                                        <Grid container direction="row" alignItems="center" spacing={3}>
+                                            <Grid item>
+                                                <SmileyIcon size={25}/>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className="expansion-panel-heading">Closing</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Grid container direction="column" justify="center">
+                                    <Grid item>
+                                        <h2>
+                                            Title goes here
+                                        </h2>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography className="expansion-panel-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius auctor tincidunt. 
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                        <Typography className="expansion-panel-text">
+                                        Vivamus vulputate ex libero, non pretium tortor eleifend non. Donec sagittis, neque eu malesuada euismod, elit nulla aliquet nibh, in laoreet eros diam quis lacus.
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" className="action-button">Action</Button>
+                                    </Grid>
+                                </Grid>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                </Grid>
+                
             </Grid> 
         );
     }
@@ -102,7 +346,7 @@ class TransactionAssist extends Component{
                     <Box component="div" paddingTop={5} paddingBottom={5}>
                         <Grid container direction="row" alignItems="center" spacing={2}>
                             <Grid item>
-                                <RepoIcon size={35} />
+                                <ArchiveIcon size={35} />
                             </Grid>
                             <Grid item>
                                 <h2 className="transaction-heading" >Transaction 1</h2>
