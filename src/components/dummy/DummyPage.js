003256195d0a9a967eea777807bb4f67bbe0d746
Akshat <sharmaakshat212000@gmail.com>
2020-06-18 09:12:24 -0500

    Sign Up component added with some styling left + invitation in New transaction form complete with select not working

--- src/components/dummy/DummyPage.js
+++ src/components/dummy/DummyPage.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { Button, Checkbox, FormControlLabel } from '@material-ui/core';
 import SignIn from '../account/SignInModal';
+import SignUpModal from '../account/SignUpModal';
 import SideDrawer from '../shared/drawer/SideDrawer';
 import ReallosLogo from '../../assets/reallos-logo.svg';
 import './DummyPage.css';
 
 function DummyPage() {
     let [signInModalVisible, setSignInModalVisibility] = useState(false);
+    let [signUpModalVisible, setSignUpModalVisibility] = useState(false);
     let [sideDrawerVisible, setSideDrawerVisibility] = useState(false);
     let [isSideDrawerLeftSide, setSideDrawerLeftSide] = useState(false);
 
@@ -17,6 +19,10 @@ function DummyPage() {
 
                 <Button variant="contained" onClick={() => setSignInModalVisibility(true)}>
                     Sign In
+                </Button>
+
+                <Button variant="contained" onClick={() => setSignUpModalVisibility(true)}>
+                    Sign Up
                 </Button>
 
                 <Button variant="contained" onClick={() => setSideDrawerVisibility(true)}>
@@ -40,6 +46,10 @@ function DummyPage() {
                 dismissCallback={() => setSignInModalVisibility(false)}
             />
 
+            <SignUpModal
+                visible={signUpModalVisible}
+                dismissCallback={() => setSignUpModalVisibility(false)}
+            />
             <SideDrawer
                 title="Hello"
                 visible={sideDrawerVisible}
