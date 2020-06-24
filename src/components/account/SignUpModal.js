dd1bd5c0177014ca7f92a97f2d01404bd616e5a9
Melvin. L. Abraham <melvinredvirus@gmail.com>
2020-06-24 11:20:25 +0530

    Minor styling changes in "Sign Up" modal

--- src/components/account/SignUpModal.js
+++ src/components/account/SignUpModal.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Modal from '../shared/modal/Modal';
 import './SignUpModal.css';
-import {ChevronRightIcon, ChevronLeftIcon,VerifiedIcon} from '@primer/octicons-react';
+import { ChevronRightIcon, ChevronLeftIcon,VerifiedIcon } from '@primer/octicons-react';
 import {
     FormGroup,
     FormControlLabel,
@@ -19,10 +19,10 @@ import {
     Typography
 } from '@material-ui/core';
 
-class SignUpModal extends Component{
-    constructor(props){
+class SignUpModal extends Component {
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             activeStep: 0
         }
 
@@ -31,127 +31,146 @@ class SignUpModal extends Component{
         this.subStep = this.subStep.bind(this);
     }
 
-    addStep(){
-        var steps = this.state.activeStep+1;
+    addStep() {
+        var steps = this.state.activeStep + 1;
         this.setState({
             activeStep: steps
         });
     }
 
-    subStep(){
-        var steps = this.state.activeStep-1;
+    subStep() {
+        var steps = this.state.activeStep - 1;
         this.setState({
             activeStep: steps
         });
     }
 
-    RenderStepsForm(){
-        if(this.state.activeStep === 0){
-            return(
-                <FormGroup>
-                    <TextField
-                        className="input-item"
-                        label="First Name"
-                        variant="outlined"
-                    />
-                    <TextField
-                        className="input-item"
-                        label="Last Name"
-                        variant="outlined"
-                    />
-                    <TextField
-                        className="input-item"
-                        label="Email"
-                        variant="outlined"
-                    />
-                    <TextField
-                        className="input-item"
-                        label="Phone"
-                        variant="outlined"
-                    />
-                    <Button variant="contained" className="big-next-button" onClick={this.addStep}>
-                        Next &nbsp;
-                        <ChevronRightIcon size={30}/>
-                    </Button>
-                </FormGroup>
+    RenderStepsForm() {
+        if(this.state.activeStep === 0) {
+            return (
+                <>
+                    <FormGroup>
+                        <TextField
+                            className="input-item"
+                            label="First Name"
+                            variant="outlined"
+                        />
+                        <TextField
+                            className="input-item"
+                            label="Last Name"
+                            variant="outlined"
+                        />
+                        <TextField
+                            className="input-item"
+                            label="Email"
+                            variant="outlined"
+                        />
+                        <TextField
+                            className="input-item"
+                            label="Phone"
+                            variant="outlined"
+                        />
+                        <Button variant="contained" className="signup-contained-button" onClick={this.addStep}>
+                            Next &nbsp;
+                            <ChevronRightIcon size={25} />
+                        </Button>
+                    </FormGroup>
+                    <div id="signin-link-footer">
+                        Already have an account? &nbsp;
+                        <a href="#">Sign In</a>
+                    </div>
+                </>
             );
         }
-        else if(this.state.activeStep === 1){
-            return(
-                <FormGroup>
-                     <FormControl variant="outlined" className="input-item">
-                        <InputLabel id="role">Role</InputLabel>
-                        <Select
-                            labelId="role"
-                            id="role_select"
-                            label="Role"
-                        ></Select>
-                    </FormControl>
-                    <FormControl variant="outlined" className="input-item">
-                        <InputLabel id="state">State</InputLabel>
-                        <Select
-                            labelId="sate"
-                            id="state_select"
-                            label="State"
-                        ></Select>
-                    </FormControl>
-                    <TextField
-                        className="input-item"
-                        label="Create Password"
-                        variant="outlined"
-                        type="Password"
-                    />
-                     <TextField
-                        className="input-item"
-                        label="Confirm Password"
-                        variant="outlined"
-                        type="Password"
-                    />
-                    <Grid container direction="row-reverse" justify="space-between" alignItems="center">
-                        <Grid item>
-                            <Button variant="contained" className="small-next-button" onClick={this.addStep}>
-                                Next &nbsp;
-                                <ChevronRightIcon />
-                            </Button>
+
+        else if (this.state.activeStep === 1) {
+            return (
+                <>
+                    <FormGroup>
+                        <FormControl variant="outlined" className="input-item">
+                            <InputLabel id="role">Role</InputLabel>
+                            <Select
+                                labelId="role"
+                                id="role_select"
+                                label="Role"
+                            ></Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className="input-item">
+                            <InputLabel id="state">State</InputLabel>
+                            <Select
+                                labelId="sate"
+                                id="state_select"
+                                label="State"
+                            ></Select>
+                        </FormControl>
+                        <TextField
+                            className="input-item"
+                            label="Create Password"
+                            variant="outlined"
+                            type="Password"
+                        />
+                        <TextField
+                            className="input-item"
+                            label="Confirm Password"
+                            variant="outlined"
+                            type="Password"
+                        />
+                        <Grid container style={{marginTop: 20}} direction="row-reverse" justify="space-between" alignItems="center">
+                            <Grid item>
+                                <Button variant="contained" className="signup-contained-button small-next-button" onClick={this.addStep}>
+                                    Next &nbsp;
+                                    <ChevronRightIcon size={25} />
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" className="signup-contained-button small-back-button" onClick={this.subStep}>
+                                    <ChevronLeftIcon size={25} />  &nbsp;
+                                    Back
+                                </Button>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Button variant="outlined" className="small-back-button" onClick={this.subStep}>
-                                <ChevronLeftIcon />  &nbsp;
-                                Back
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </FormGroup>
+                    </FormGroup>
+                    <div id="signin-link-footer">
+                        Already have an account? &nbsp;
+                        <a href="#">Sign In</a>
+                    </div>
+                </>
             );
         }
-        else{
-            return(
-                <Grid direction="column" justify="center" alignItems="center">
+
+        else {
+            return (
+                <Grid
+                    className="signup-awaiting-verification-container"
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
                     <Grid item>
                         <Grid container direction="row" justify="center">
                             <VerifiedIcon size={100} className="verified-icon"/>
                         </Grid>
                     </Grid>
-                    <Grid item>
-                        <Grid container direction="row" justify="center">
-                            <Typography className="awaiting-heading">Awaiting Verification</Typography>
+                    <div className="signup-awaiting-verification-textblock">
+                        <Grid item>
+                            <Grid container direction="row" justify="center">
+                                <Typography className="awaiting-heading">Awaiting Verification</Typography>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                    <Grid item>
-                        <Grid container direction="row" justify="center">
-                            <Typography align="justify">We have sent a link in your mail address, please click the link so that we can verify your email address and activate your account.</Typography>
+                        <Grid item>
+                            <Grid container direction="row" justify="center">
+                                <Typography align="justify">We have sent a link in your mail address, please click the link so that we can verify your email address and activate your account.</Typography>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </div>
                 </Grid>
             );
         }
     }
 
-
-
-    render(){
-        return(
-            <Modal title="Sign UP" visible={this.props.visible} dismissCallback={this.props.dismissCallback}>
+    render() {
+        return (
+            <Modal title="Sign Up" visible={this.props.visible} dismissCallback={this.props.dismissCallback}>
                 <Stepper activeStep={this.state.activeStep}>
                     <Step>
                         <StepLabel />
