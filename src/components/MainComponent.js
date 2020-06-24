5a758462c7a914bbc290dd9ad095a102a71b60eb
Akashnemade21 <akashnemade21@gmail.com>
2020-06-24 23:50:40 +0530

    chat app added | styling is remaining

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React from "react";
 import TransactionDashbaord from "./transaction_dashboard/TransactionDasboardComponent";
-import TransactionAssist from './transaction_assist/TransactionAssistComponent';
-import PaperWork from './paperwork/PaperWorkComponent';
+import TransactionAssist from "./transaction_assist/TransactionAssistComponent";
+import PaperWork from "./paperwork/PaperWorkComponent";
 import DummyPage from "./dummy/DummyPage";
-import PeopleInvolved from "./people_involved/PeopleInvolved"
-import Todo from './todo/TodoComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import PeopleInvolved from "./people_involved/PeopleInvolved";
+import Todo from "./todo/TodoComponent";
+import ChatMain from "./chat/ChatMain/ChatMain";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 
 function Main(props) {
-    // All the routes must be configured here
+  // All the routes must be configured here
 
-    return(
-        <div>
-            <Switch>
-                <Route path="/dummy" component={DummyPage} />
-                <Route path="/transaction" component={TransactionDashbaord} />
-                <Route path="/transaction_assist" component ={TransactionAssist} />
-                <Route path="/paperwork" component={PaperWork} />
-                <Route path="/people" component={PeopleInvolved} />
-                <Route path="/todo" component={Todo} />
-                <Redirect to="/dummy" />
-            </Switch>
-        </div>
-    );
+  return (
+    <div>
+      <Switch>
+        <Route path="/dummy" component={DummyPage} />
+        <Route path="/transaction" component={TransactionDashbaord} />
+        <Route path="/transaction_assist" component={TransactionAssist} />
+        <Route path="/paperwork" component={PaperWork} />
+        <Route path="/people" component={PeopleInvolved} />
+        <Route path="/todo" component={Todo} />
+        <Route path="/chat" render={(props) => <ChatMain {...props} />} />
+        <Redirect to="/dummy" />
+      </Switch>
+    </div>
+  );
 }
 
 export default withRouter(Main);
