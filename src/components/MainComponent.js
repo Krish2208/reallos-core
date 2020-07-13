ab1cc58132440082a5a3622d7e48fcb2aab40b1d
Melvin. L. Abraham <melvinredvirus@gmail.com>
2020-07-13 17:19:28 +0530

    Added Error 404 Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,9 +7,9 @@ import DummyPage from "./dummy/DummyPage";
 import PeopleInvolved from "./people_involved/PeopleInvolved";
 import Todo from "./todo/TodoComponent";
 import DiscussionsMain from "./discussions/DiscussionsMain/DiscussionsMain";
+import Error404Component from './404/Error404Component';
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import Auth from "./account/Authenticate";
-
 
 const PrivateRoute = (
   { component: Component, ...rest } // Component that protects all the routing if the user is not autenticated
@@ -40,7 +40,9 @@ function Main(props) {
         <PrivateRoute path="/transaction/:tid/people" component={PeopleInvolved} />
         <PrivateRoute path="/transaction/:tid/todo" component={Todo} />
         <PrivateRoute path="/transaction/:tid/discussions" component={DiscussionsMain} />
-        <Redirect to="dummy"/>
+        {/* <Redirect path="/transaction/:tid" to="assist" /> */}
+        <Redirect exact from="/" to="dummy" />
+        <Route path="*" component={Error404Component} />
       </Switch>
     </div>
   );
