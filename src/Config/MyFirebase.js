c885646080cd78977cf0712fa1504257a559b2f4
Krish2208 <diwankrish.17@gmail.com>
2020-07-15 14:37:17 +0530

    Configured with New Firebase Account

--- src/Config/MyFirebase.js
+++ src/Config/MyFirebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyCoMC83X66Ax_eDjQcTKtYd9eTbkiDzp58",
-  authDomain: "reallos-test.firebaseapp.com",
-  databaseURL: "https://reallos-test.firebaseio.com",
-  projectId: "reallos-test",
-  storageBucket: "reallos-test.appspot.com",
-  messagingSenderId: "361319907913",
-  appId: "1:361319907913:web:51c50386396e2f8b6bf598"
+  apiKey: "AIzaSyBhN8rB3qXmFG_6TmVABtgLIMzLA-4-5yI",
+  authDomain: "reallos-382c7.firebaseapp.com",
+  databaseURL: "https://reallos-382c7.firebaseio.com",
+  projectId: "reallos-382c7",
+  storageBucket: "reallos-382c7.appspot.com",
+  messagingSenderId: "335659848409",
+  appId: "1:335659848409:web:e8c99b76c4c161af9724c8",
+  measurementId: "G-F60B6S0CXV"
 };
 
 firebase.initializeApp(config);
