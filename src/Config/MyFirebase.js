158fe5f016e58f74aac4e489c4307e5aa49bce18
Krish Agrawal <32320437+Krish2208@users.noreply.github.com>
2022-02-08 10:05:53 +0530

    Update MyFirebase.js

--- src/Config/MyFirebase.js
+++ src/Config/MyFirebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyBhN8rB3qXmFG_6TmVABtgLIMzLA-4-5yI",
-  authDomain: "reallos-382c7.firebaseapp.com",
-  databaseURL: "https://reallos-382c7.firebaseio.com",
-  projectId: "reallos-382c7",
-  storageBucket: "reallos-382c7.appspot.com",
-  messagingSenderId: "335659848409",
-  appId: "1:335659848409:web:e8c99b76c4c161af9724c8",
-  measurementId: "G-F60B6S0CXV"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(config);
